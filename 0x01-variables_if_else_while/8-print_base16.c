56051222c8a7b26490b5de2cb097cf07e9e18727
rhf007 <rahofahazem512@gmail.com>
2023-04-23 18:26:19 -0700

    print base16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - print all base16 numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(num);
+	}
+
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(num);
+	}
+
+	putchar('\n');
+	return (0);
+}
