aa2aa99155959b56bd08671abea57cd7bc631488
rhf007 <rahofahazem512@gmail.com>
2023-04-23 20:07:13 -0700

    print every possible combination of 2 digits with putchar

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+/**
+ * main - print all possible combinations of 2 digits with putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 48; num1 <= 56; num1++)
+	{
+		for (num2 = 49; num2 <= 57; num2++)
+		{
+			if (num2 > num1)
+			{
+				putchar(num1);
+				putchar(num2);
+				if (num1 != 56 || num2 != 57 )
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
