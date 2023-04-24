7328b5553b285a33b37222b2c731809960c3e49b
rhf007 <rahofahazem512@gmail.com>
2023-04-23 17:45:34 -0700

    print all single base10 numbers starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - print all base10 single digit numbers starting from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf(num);
+	}
+
+	putchar('\n');
+	return (0);
+}
