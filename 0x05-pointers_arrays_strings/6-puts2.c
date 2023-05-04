43dcf53577a7e8a8078893a24948d85a5e58a6f2
rhf007 <rahofahazem512@gmail.com>
2023-05-04 02:04:30 -0700

    print every other character in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -17,9 +17,8 @@ void puts2(char *str)
 		str++;
 	}
 
-	for (i = 0; i <= lencount; i += 2)
+	for (i = 0; i < lencount; i = i + 2)
 	{
-		_putchar(str[i]);
-		s[i/2] = str[i];
+		_putchar(*(str + i));
 	}
 }
