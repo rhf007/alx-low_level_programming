4874441002fcbdf2c47320a3ca39853801201606
rhf007 <rahofahazem512@gmail.com>
2023-05-05 08:29:37 -0700

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ * cap_string - capitalize string words
+ * @s: string
+ * Return: capitalized string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+		{
+			i++;
+		}
+
+		if (s[i] == ' ' || s[i] == '	' || s[i] == '\n'
+		   || s[i] == ',' || s[i] == ';' || s[i] == '.'
+		   || s[i] == '?' || s[i] == '!' || s[i] == '"'
+		   || s[i] == '(' || s[i] == ')' || s[i] == '{'
+		   || s[i] == '}' || i == 0)
+		{
+			s[i] -=  32;
+		}
+
+		i++;
+	}
+
+	return (s);
+}
