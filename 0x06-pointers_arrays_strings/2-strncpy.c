73ef064b8d4758402ac362cf74efc6e87b555254
rhf007 <rahofahazem512@gmail.com>
2023-05-05 05:55:45 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,16 +7,19 @@
  * @n: limit
  * Return: pointer to dest
  */
-_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
+	int i;
+
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	for (i ; i < n; i++)
+	while (i < n)
 	{
 		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
