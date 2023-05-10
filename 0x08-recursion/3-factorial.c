c181405ca1806cc24e06881d6108cf68df8a6152
rhf007 <rahofahazem512@gmail.com>
2023-05-10 09:11:00 -0700

    Recursive Factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,13 +3,14 @@
 /**
  * factorial - the factorial of a given number
  * @n: the number
- * Return: factorial of n. -1 if n is negative, 1 if n is 0 or 1, factorial otherwise
+ * Return: factorial of n. -1 if n is negative, 1 if n is 0 or 1,
+ * normal factorial otherwise
  */
 int factorial(int n)
 {
 	if (n < 0)
 	{
-		return -1;
+		return (-1);
 	}
 
 	if (n == 0 || n == 1)
