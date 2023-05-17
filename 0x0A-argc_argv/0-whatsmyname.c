0ef49894ad318c884cd90dce21cde2e32f83c90c
rhf007 <rahofahazem512@gmail.com>
2023-05-17 07:25:04 -0700

    Print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: command-line arguments
  * Return: 0 on success
  */
-int main (__attribute__((unused)) int argc, char const *argv[])
+int main(__attribute__((unused)) int argc, char const *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
