030ff68e6037e5705e9f374a518b34a07e8dcc5d
rhf007 <rahofahazem512@gmail.com>
2023-05-21 10:42:09 -0700

    calloc using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _memset - fills the first n bytes of the memory area pointed to by s with
+ * the constant byte b
+ * @s: string
+ * @b: constatnt byte/character
+ * @n: number of bytes
+ * Return: a pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * _calloc - allocate memory for an array of nmemb elements of size bytes each,
+ * using malloc. The memory is set to zero
+ * @nmemb: number of elements in array
+ * @size: size of each element
+ * Return: a pointer to the allocated memory(SUCCESS). If nmemb or size is 0,
+ * then _calloc returns NULL/If malloc fails, then _calloc returns NULL(FAIL)
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array = malloc(size * nmemb);
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	_memset(array, 0, (nmemb * size));
+
+	return (array);
+}
