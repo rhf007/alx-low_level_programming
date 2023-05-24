783669578c53b25b389b9e9b31a5e15f06fe7569
rhf007 <rahofahazem512@gmail.com>
2023-05-24 09:18:39 -0700

    print_dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - print a struct dog
+ * @d: struct dog to be printed
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		d = malloc(sizeof(struct dog));
+	}
+
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+
+	if (d-> age == 0)
+	{
+		printf("Age: (nil)\n");
+	}
+
+	if (d-> owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d-> name, d-> age, d-> owner);
+}
