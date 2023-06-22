dee85cfc5c935dfee4b92987f35087e72588ecec
rhf007 <rahofahazem512@gmail.com>
2023-06-21 19:59:33 -0700

    Find Element

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - a function that searches for an integer.
+ * @array: array of integers to be searched
+ * @size: array size
+ * @cmp: pointer to the function to be used to compare values
+ * Return: index of element (if cmp != 0),
+ * -1 otherwise (no element, size <= 0, etc)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, found;
+
+	if (array != NULL && size != 0 && cmp != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			found = cmp(array[i]);
+			if (found != 0)
+			{
+				break;
+			}
+		}
+
+		return (i);
+	}
+
+	else
+	{
+		return (-1);
+	}
+}
