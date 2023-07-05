260379a2cb3dac6778fb926d73b6c1638473b65a
rhf007 <rahofahazem512@gmail.com>
2023-07-05 12:37:41 -0700

    Insert at Node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = newnode;
 		return (newnode);
 	}
-	while (ptr != NULL && i != (idx - 1))
+	while (ptr != NULL && i != idx)
 	{
 		ptr = ptr->next;
 		i++;
