685243ec165e41c7bfa294c17055175ccba2d49c
rhf007 <rahofahazem512@gmail.com>
2023-07-13 11:21:57 -0700

    Create Reality

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY |O_TRUNC, 0600);
 	bytes = write(fd, text_content, len);
 
 	if (fd == -1 || bytes == -1)
