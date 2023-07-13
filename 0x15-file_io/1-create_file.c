debaef7e6a2006ac4e794c3ce856a560848b2cd2
rhf007 <rahofahazem512@gmail.com>
2023-07-13 11:16:02 -0700

    Create Reality

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: file name
+ * @text_content: a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ * The created file must have those permissions: rw-------. If the file already
+ * exists, do not change the permissions.
+ * if the file already exists, truncate it
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes, len;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+		{
+			;
+		}
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	bytes = write(fd, text_content, len);
+
+	if (fd == -1 || bytes == -1)
+	{
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
