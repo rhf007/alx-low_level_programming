18209eab30dd5b98a3c9085ab60fee34a27522ab
rhf007 <rahofahazem512@gmail.com>
2023-08-23 15:20:51 -0700

    Hard Lesson on Conditions

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,7 +9,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current = *head;
-	
+
 	if (*head == NULL)
 	{
 		return (-1);
@@ -42,5 +42,5 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		}
 	}
 	free(current);
-	return(1);
+	return (1);
 }
