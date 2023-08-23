a57541b3ca62e4199665419891d16ee581af6872
rhf007 <rahofahazem512@gmail.com>
2023-08-23 15:19:08 -0700

    Hard Lesson on Conditions

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list.
+ * @head: list/pointer to the head of the list
+ * @index: index to delete at
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+	
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	for (; index != 0; index--)
+	{
+		if (current == NULL)
+		{
+			return (-1);
+		}
+		current = current->next;
+	}
+
+	if (current == *head)
+	{
+		*head = current->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+	}
+
+	else
+	{
+		current->prev->next = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = current->prev;
+		}
+	}
+	free(current);
+	return(1);
+}
